aacdd32235561a013b5234a7c8a5230edbed234b
ujeongeom <110080718+ujeongeom@users.noreply.github.com>
2024-02-06 16:56:21 +0900

    Update k8s_sample.py

--- dags/k8s_sample.py
+++ dags/k8s_sample.py
@@ -2,9 +2,12 @@ import datetime
 
 from airflow import DAG
 from kubernetes.client import models as k8s
-#from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import (
-#    KubernetesPodOperator,
-#)
+
+import os
+import sys
+
+# 사전에 mount 한 POD의 특정 폴더를 append 한다.  
+sys.path.append('/bitnami/airflow/kube-config') # kube_config
 
 from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
 
@@ -21,14 +24,17 @@ with DAG(
 
     run_python = KubernetesPodOperator(
         task_id="run_python_script",
-        name="run_python_script",
-        namespace="airflow",
+        name="run_python_edu",
+        in_cluster=False,  # false로 한다. 
+        cluster_context='k3s-test', # context 를 설정한다. context 가 하나면 설정 안해도 됨
+        config_file='/bitnami/airflow/kube-config/config_yujeong', # kube config 화일을 설정한다.
+        namespace="default", #  namespace를 설정한다.
         image="python:3.10-slim",  # 이 이미지에 필요한 파이썬 스크립트와 의존성이 포함되어 있어야 합니다.
-        is_delete_operator_pod=True,
+        is_delete_operator_pod=False,
         cmds=["python", "-c"],
         arguments=[
             'print("Hello, World!")'
         ],  # 여기에 파이썬 스크립트를 입력하거나 실행할 파이썬 파일의 경로를 제공하세요.
         get_logs=True,
-        # resources=resources,
+        container_resources=resources,
     )
